368bd4b8bc43664d4917ba89250d118a985a606c
Michael White <hrt666.cs@gmail.com>
2024-12-23 08:14:31 +0900

    Added shared/functions.php adding db_open() as a shared function for database connections

--- shared/functions.php
+++ shared/functions.php
@@ -8,3 +8,16 @@
 function str2html(string $arg_input) :string {
     return htmlspecialchars($arg_input, ENT_QUOTES, 'UTF-8');
 }
+
+function db_open(){
+    $dsn = "mysql:host=localhost;dbname=sample_db";
+    $user = "phpuser";
+    $password = "dummy";
+    $options = [
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_EMULATE_PREPARES   => false,
+        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
+    ];
+    $dbh = new PDO($dsn, $user, $password, $options);
+    return $dbh;
+}
