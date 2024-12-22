9af40cb75691a2a5673cc4addb9cd472351c1f80
Michael White <hrt666.cs@gmail.com>
2024-12-23 08:24:26 +0900

    Added lesson5/list4.php to display the result of an UPDATE statement in HTML format

---
+++ lesson5/list4.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang='ja'>
+    <head>
+            <meta charset='UTF-8'> 
+            <title>Sample Code</title>
+            <link rel='stylesheet' type='text/css' href='../shared/style.css'>
+    </head>
+<body>
+<header>
+    <h1>Book Database</h1>
+</header>
+
+<?php
+require_once '../shared/functions.php'; // with a function for XSS (str2html)
+try {
+    // Establish a database connection using db_open() from /shared/functions.php
+    $dbh = db_open();
+    $sql = 'SELECT * FROM books';
+    $statement = $dbh->query($sql);
+?>
+
+<table>
+    <tr><th>Title</th><th>ISBN</th><th>Price</th><th>Publish Date</th><th>Author</th>
+    <?php while ($row = $statement->fetch()): ?>
+    <tr>
+        <td><?php echo str2html($row['title']) ?></td>
+        <td><?php echo str2html($row['isbn']) ?></td>
+        <td><?php echo str2html($row['price']) ?></td>
+        <td><?php echo str2html($row['publish']) ?></td>
+        <td><?php echo str2html($row['author']) ?></td>
+    </tr>
+    <?php endwhile; ?>
+</table>
+
+<?php
+}
+catch(PDOException $e){
+    echo "Error!!: " . str2html($e->getMessage()) . "<br>";
+    //$e->getMessage() for a training purpose to know how it works
+    exit;
+}
+?>
+</body>
+</html>
