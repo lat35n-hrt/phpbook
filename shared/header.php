32a09c83a8673d2ee168280ee6383bd28a0de63d
Michael White <hrt666.cs@gmail.com>
2024-12-25 08:35:43 +0900

    Added shared/header.php as a shared header

---
+++ shared/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang='ja'>
+    <head>
+            <meta charset='UTF-8'> 
+            <title>Book Database</title>
+            <link rel='stylesheet' type='text/css' href='../shared/style.css'>
+    </head>
+<header>
+    <h1>Book List</h1>
+</header>
+<div>
+        <ul id='nav'>
+            <li><a href='./'>Home</a></li>
+            <li><a href='./input.php'>Home</a></li>
+            <li><a href='./logout.php'>Logout</a></li>
+        </ul>
+</div>
