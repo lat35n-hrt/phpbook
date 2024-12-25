def555cf0044c8dff82b3656df9d9e29b0a74417
Michael White <hrt666.cs@gmail.com>
2024-12-25 11:15:08 +0900

    Added lesson5/input.php renaming add.html with a shared header and a shared footer

---
+++ lesson5/input.php
@@ -0,0 +1,32 @@
+<?php include_once __DIR__ . '/../shared/header.php'; // with a shared header ?>
+<form action='add5.php' method='post'>
+    <p>
+        <label for='title'>Title (Required Max200 Characters): </label>
+        <input type='text' name='title'>
+    </p>
+
+    <p>
+        <label for='isbn'>ISBN (Max 13 Characters): </label>
+        <input type='text' name='isbn'>
+    </p>
+
+    <p>
+        <label for='price'>Price (Max 6 Digits): </label>
+        <input type='text' name='price'>
+    </p>
+
+    <p>
+        <label for='publish'>Publish Date (YYYY-MM-DD): </label>
+        <input type='text' name='publish'>
+    </p>
+
+    <p>
+        <label for='author'>Author (Max 80 Characters): </label>
+        <input type='text' name='author'>
+    </p>
+
+    <p class='button'>
+        <input type='submit' value='Submit'>
+    </p>
+</form>
+<?php include_once __DIR__ . '/../shared/footer.php'; // with a shared header
