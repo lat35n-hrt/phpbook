fbafac38d616e9185c607470b4e8496cf6e7a9ab
Michael White <hrt666.cs@gmail.com>
2024-12-29 08:45:18 +0900

    feat: added require_once for shared/token_check.php

--- lesson6/add.php
+++ lesson6/add.php
@@ -2,6 +2,7 @@
 require_once __DIR__ . '/../shared/functions.php'; // with a function for XSS (str2html) and db_open()
 include_once __DIR__ . '/../shared/error_check.php'; // with validations for error check
 include_once __DIR__ . '/../shared/header.php'; // with a shared header
+require_once __DIR__ . '/../shared/token_check.php'; // with a token check
 
 // Extract year, month, and day from the input 'YYYY-MM-DD'
 $date = explode('-', $_POST['publish']);
